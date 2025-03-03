e352e7ee946b28f95212df1875114e74aa6aec99
CopperGroup <coppergroupstudio@gmail.com>
2025-03-03 12:03:25 +0100

    Update store.js

--- constants/store.js
+++ constants/store.js
@@ -2,5 +2,6 @@ export const Store = {
     name: "G&M",
     currency: "UAH",
     currency_sign: "₴",
-    domain: "https://fo-scandinavia.vercel.app"
+    domain: "https://fo-scandinavia.vercel.app",
+    database: "GandM"
 }
